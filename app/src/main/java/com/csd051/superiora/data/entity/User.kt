ef99b898cda59612b5bd4a90e91d827e8b0e3843
Nada Ika Sari <nadaikasari01@gmail.com>
2021-12-07 11:37:55 +0700

    add database

--- app/src/main/java/com/csd051/superiora/data/entity/User.kt
+++ app/src/main/java/com/csd051/superiora/data/entity/User.kt
@@ -1,12 +1,29 @@
 package com.csd051.superiora.data.entity
 
 import android.os.Parcelable
+import androidx.annotation.NonNull
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
+@Entity(tableName = "user")
 data class User(
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "id")
+    var id: Int,
+
+    @ColumnInfo(name = "nama")
     var nama : String,
+
+    @ColumnInfo(name = "email")
     var email : String,
+
+    @ColumnInfo(name = "password")
     var password : String,
+
+    @ColumnInfo(name = "photo")
     var photo : String
 ) : Parcelable
--- app/src/main/java/com/csd051/superiora/data/room/SuperioraDatabase.kt
+++ app/src/main/java/com/csd051/superiora/data/room/SuperioraDatabase.kt
@@ -1,8 +1,28 @@
 package com.csd051.superiora.data.room
 
+import android.content.Context
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 // Todo Buat database
 abstract class SuperioraDatabase : RoomDatabase() {
+    abstract fun superioraDao() : SuperioraDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: SuperioraDatabase? = null
+
+        fun getInstance(context: Context): SuperioraDatabase =
+            INSTANCE ?: synchronized(this) {
+                Room.databaseBuilder(
+                    context.applicationContext,
+                    SuperioraDatabase::class.java,
+                    "superiora.db"
+                ).fallbackToDestructiveMigration()
+                    .build().apply {
+                        INSTANCE = this
+                    }
+            }
+    }
 
 }
